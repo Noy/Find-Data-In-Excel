78456782030686b7b87b01e763afe4887ba37be6
Noy <noyhillel@gmail.com>
2017-03-06 15:15:24 +0000

    changes, no more errors or weird occurrences..

--- excel.go
+++ excel.go
@@ -6,53 +6,79 @@ import (
 	"bufio"
 	"os"
 	"strings"
-	"path/filepath"
-	"time"
+	"io/ioutil"
 )
 
 func main() {
-	findData()
-}
-
-func findData() {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Enter the Data:")
+	fmt.Println("Enter the Data string:")
 	nextText, _ := reader.ReadString('\n')
-	searchDir := "."
-	fileList := []string{}
-	err := filepath.Walk(searchDir, func(path string, f os.FileInfo, err error) error {
-		fileList = append(fileList, path)
-		return nil
-	})
-	var cellTextString string
-	var fileTextString string
-	for _, file := range fileList {
-		if !strings.HasSuffix(file, "xlsx") {continue}
-		if strings.HasPrefix(file, "~") {continue}
-		xlFile, err := xlsx.OpenFile(file)
-		if err != nil { handleError(err) }
+	files, err := ioutil.ReadDir(".")
+	if err != nil { handleError(err) }
+	for _, file := range files {
+		if strings.HasPrefix(file.Name(), "~") {continue}
+		if !strings.HasSuffix(file.Name(), ".xlsx") {continue}
+		xlFile, _ := xlsx.OpenFile(file.Name())
 		for _, sheet := range xlFile.Sheets {
 			for _, row := range sheet.Rows {
 				for _, cell := range row.Cells {
-					cellText, _ := cell.String()
-					if strings.Contains(nextText, cellText) {
-						fileTextString = file
-						cellTextString = "*** " + cellText + " ***"
+					if cell.Value == strings.ToLower(strings.Replace(nextText, "\n", "", -1)) {
+						fmt.Println("That pattern occured in: " + file.Name())
 					}
 				}
-				time.Sleep(2)
 			}
 		}
-		fmt.Println("Searching: " + fileTextString + " for cell: " + cellTextString)
-	}
-	if err != nil {
-		handleError(err)
 	}
 }
 
 func handleError(err error) {
 	fmt.Println("Something went wrong! Check below:")
 	fmt.Println(err)
+}
+
+//// OLD STUFF
+
+func holdOn() {
+	//xlFile, _ := xlsx.OpenFile(strings.Replace(file.Name(), "\n", "", -1))
+	//for _, sheet := range xlFile.Sheets {
+	//	for _, row := range sheet.Rows {
+	//		for _, cell := range row.Cells {
+	//			if !strings.HasSuffix(file.Name(), "xlsx") {continue}
+	//			if strings.HasPrefix(file.Name(), "~") {continue}
+	//			cellText, _ := cell.String()
+	//			if strings.Contains(s, cellText) {
+	//				fmt.Println(s + " is there")
+	//			}
+	//		}
+	//	}
+	//}
+}
+
+func findData() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the Data string:")
+	nextText, _ := reader.ReadString('\n')
+	fmt.Println("Enter the File name:")
+	text, _ := reader.ReadString('\n')
+	files, err := ioutil.ReadDir(".")
+	for _, file := range files {
+		fmt.Println(file)
+	}
+	excelFilename := text
+	xlFile, err := xlsx.OpenFile(strings.Replace(excelFilename, "\n", "", -1))
+	if err != nil { handleError(err) }
+	for _, sheet := range xlFile.Sheets {
+		for _, row := range sheet.Rows {
+			for _, cell := range row.Cells {
+				if !strings.HasSuffix(excelFilename, "xlsx") {continue}
+				if strings.HasPrefix(excelFilename, "~") {continue}
+				cellText, _ := cell.String()
+				if strings.Contains(nextText, cellText) {
+					fmt.Println(nextText + " is there")
+				}
+			}
+		}
+	}
 }
 
 func searchInstead() {
